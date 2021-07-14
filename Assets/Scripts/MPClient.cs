0865ba5476f5ccaec4d3c319c9991dac52280dbb
Neymik <Neymik@users.noreply.github.com>
2021-07-14 19:56:36 +0300

    mp

--- Assets/Scripts/MPClient.cs
+++ Assets/Scripts/MPClient.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MPClient : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+[RequireComponent( typeof( NetworkView ) )] // сообщает Unity о том, что нам нужен компонент NetworkView. Данному компоненту NetworkStateSynchronization можно выставить Off.
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+public class MPClient : MonoBehaviour {
+    public GameObject playerPrefab; // префаб игрока, который будет создан в процессе игры
+
+    [RPC] // сообщает Unity о том, что данный метод можно вызвать из сети
+    private void baseAction( int x, int y ) {
+
+        Vector2 pos = new Vector2();
+        pos.x = x;
+        pos.y = y;
+
+        Spawn (pos);
+    }
+	
+	void OnDisconnectedFromServer( NetworkDisconnection info ) {
+        Network.DestroyPlayerObjects( Network.player ); // удаляемся из игры
     }
 }
--- Assets/Scripts/MPServer.cs
+++ Assets/Scripts/MPServer.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MPServer : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+[RequireComponent( typeof( NetworkView ) )] // сообщает Unity о том, что нам нужен компонент NetworkView. Данному компоненту NetworkStateSynchronization можно выставить Off.
+
+public class MPServer : MonoBehaviour {
+	private int playerCount = 0; // хранит количество подключенных игроков
+	public int PlayersCount { get { return playerCount; } } // публичный доступ для внешних компонентов относительно количества игроков на сервере
+
+    void OnServerInitialized() {
+        // SendMessage( "SpawnPlayer", "Player Server" ); // создаем локального игрока сервера
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnPlayerConnected( NetworkPlayer player ) {
+		++playerCount;
+        // networkView.RPC( "SpawnPlayer", player, "Player " + playerCount.ToString() ); // вызываем у игрока процедуру создания экземпляра префаба
+    }
+
+    void OnPlayerDisconnected( NetworkPlayer player ) {
+        --playerCount; // уменьшаем количество игроков
+        Network.RemoveRPCs( player ); // очищаем список процедур игрока
+        Network.DestroyPlayerObjects( player ); // уничтожаем все объекты игрока
     }
 }
